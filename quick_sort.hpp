f30de4973753caea02f21eba32322f69d7bfead8
7081 <tobiaszlewicki8@gmail.com>
2026-06-22 12:12:24 +0200

    fix quicksort

--- quick_sort.hpp
+++ quick_sort.hpp
@@ -5,18 +5,40 @@
 
 using std::swap;
 
-template <typename T> const T& median_of_three(const T& a, const T& b, const T& c) {
-        if ((a <= b && b <= c) || (a >= b && b >= c)) return b;
-        if ((b <= a && a <= c) || (b >= a && a >= c)) return a;
-        return c;
-    }
+template <typename T>
+const T& median_of_three(const T& a, const T& b, const T& c) {
+    if ((a <= b && b <= c) || (a >= b && b >= c)) return b;
+    if ((b <= a && a <= c) || (b >= a && a >= c)) return a;
+    return c;
+}
 
-template <typename T> int partition(T* array, int left, int right){
-    T pivot = median_of_three(array[left], array[(left + right) / 2], array[right]);
+template <typename T>
+int partition(T* array, int left, int right) {
+
+    int mid = (left + right) / 2;
+
+    int median_index;
+    const T& median = median_of_three(
+        array[left],
+        array[mid],
+        array[right]
+    );
+
+    if (&median == &array[left])
+        median_index = left;
+    else if (&median == &array[mid])
+        median_index = mid;
+    else
+        median_index = right;
+
+    swap(array[median_index], array[right]);
+
+    T pivot = array[right];
+
     int i = left - 1;
 
-    for(int j = left; j < right; j++){
-        if (array[j] <= pivot){
+    for (int j = left; j < right; ++j) {
+        if (array[j] <= pivot) {
             ++i;
             swap(array[i], array[j]);
         }
@@ -26,9 +48,12 @@ template <typename T> int partition(T* array, int left, int right){
     return i + 1;
 }
 
-template <typename T> void quick_sort(T* array, int left, int right){
+template <typename T>
+void quick_sort(T* array, int left, int right) {
     if (right <= left) return;
+
     int pivot = partition(array, left, right);
-    quick_sort<T>(array, left, pivot-1);
-    quick_sort<T>(array, pivot+1, right);
+
+    quick_sort(array, left, pivot - 1);
+    quick_sort(array, pivot + 1, right);
 }
